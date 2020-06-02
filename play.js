3be9b287164130d34bbc2ca2b48d88df8cf87e13
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-02 13:13:40 -0300

    C02A12 - Refreshing the Core Syntax

---
+++ play.js
@@ -0,0 +1,9 @@
+var name = "Max";
+var age = 29;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobbies) {
+    return ("Name: " + userName + " Age: " + userAge + " Has Hobbies: " + userHasHobbies);
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
