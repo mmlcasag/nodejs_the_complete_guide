2c703bf28032fb809a7573f2800a863b882e150d
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-02 13:17:56 -0300

    C02A13 - Let & Const

--- play.js
+++ play.js
@@ -1,3 +1,4 @@
+/* LEVEL 1
 var name = "Max";
 var age = 29;
 var hasHobbies = true;
@@ -7,3 +8,12 @@ function summarizeUser(userName, userAge, userHasHobbies) {
 }
 
 console.log(summarizeUser(name, age, hasHobbies));
+*/
+
+// LET AND CONST
+// var is outdated, now you should use let or const
+const name = "Max"; // variables that must not change --> constants
+let age = 29 // variables that may change
+let hasHobbies = true; // same thing as above
+
+// name = "Marcio"; // this raises an error because it is a constant;
