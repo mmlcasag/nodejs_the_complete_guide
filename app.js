20bda47ae077b1511a5c2301e1b77e74a2fe69eb
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-02 19:42:08 -0300

    C03A30 - Sending Responses

--- app.js
+++ app.js
@@ -1,53 +1,6 @@
-// node.js core modules:
-
-// http  --> launch a server, send requests
-// https --> launch a ssl
-// fs    --> working with text files: reading and writing
-// path  --> a helper to work with url between different operating systems
-// os    --> 
-
-/*
-// imports the http module
-const http = require('http');
-
-// this function will respond to whatever requests reach our server
-
-function requestListener(req, res) {
-    console.log(req);
-}
-
-// this is our server
-const server = http.createServer(requestListener);
-
-server.listen(3000);
-*/
-
-/////////////////////////////////////////////
-
-// or you can create an anonymous function
-// this means the same
-/*
-// imports the http module
-const http = require('http');
-
-const server = http.createServer(function(req, res) {
-    console.log(req);
-});
-
-server.listen(3000);
-*/
-
-/////////////////////////////////////////////
-
-// or you can even do that:
-
-// imports the http module
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // logs the whole request
-    // with all its irrelevant attributes
-    // console.log(req);
     // what really matters is this:
     console.log('URL: ' + req.url);
     console.log('Method: ' + req.method);
@@ -57,14 +10,19 @@ const server = http.createServer((req, res) => {
     // log it separately to see its contents
     console.log(req.headers);
 
-    // this breaks the event loop
-    // process.exit();
+    // how to send responses
 
-    // Want to quit your running Node.js server?
-    // You can always do that by pressing CTRL + C in the terminal/ command prompt window 
-    // where you started your server (i.e. where you ran node app.js).
+    // set the header
+    res.setHeader('Content-Type', 'text/html');
+    // write the message
+    res.write('<html>');
+    res.write('<head><title>Hello from the server!</title></head>');
+    res.write('<body><h1>Hello from the server!</h1></body>');
+    res.write('</html>');
+    // tell the server this is it
+    res.end();
+    // be careful to not send any other response to the server after res.end()
+    // otherwise this will raise an error!
 });
 
 server.listen(3000);
-
-// we will use this third approach during this course
