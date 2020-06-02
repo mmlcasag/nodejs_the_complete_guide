fdaa8edd7bcb475325e43f345e9410c530b8c473
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-02 17:20:12 -0300

    C02A18 - Understanding Spread and Rest Operators

--- play.js
+++ play.js
@@ -134,7 +134,7 @@ console.log(newerHobbies);
 console.log(newHobbies);
 console.log(hobbies);
 */
-
+/*
 // Objects and Arrays are so-called "Reference Types"
 
 // if hobbies is a const, why can I push new elements inside the array?
@@ -146,4 +146,58 @@ console.log(hobbies);
 // so even though we are adding another element, the pointer is still the same
 // this means that arrays are reference types
 // everything is reference types, except numbers, strings, and booleans, basically
+*/
 
+// Spread and Rest Operators
+
+// Spread Operator
+
+// copy an array
+const hobbies = ['Sports', 'Cooking'];
+
+const array2 = hobbies.slice(); // copies an array
+const array3 = [hobbies]; // what do I see in the console? 
+                          // is an array with another array in it
+                          // this is not a copy
+const spreadArray = [...hobbies]; // pulls out all the elements of the array and push it to the new array
+const spreadArrayAndMore = [...hobbies, 'Programming'] // we can copy and also and more elements at the same time
+
+console.log(hobbies);
+console.log(array2);
+console.log(array3);
+console.log(spreadArray);
+console.log(spreadArrayAndMore);
+
+// we can also spread objects
+
+const person = {
+    name: 'Marcio',
+    age: 31
+}
+
+const newPerson = {...person};
+
+console.log(person);
+console.log(newPerson);
+
+// Rest operator
+
+const toArray = (arg1, arg2, arg3) => {
+    return [arg1, arg2, arg3];
+}
+
+console.log(toArray('Kiko', 'Leandro', 'Bruno'));
+
+// but what if I want to add infinite arguments to that function?
+
+const toArrayRestOperator = (...args) => {
+    return args;
+}
+
+// you can also use it like that
+
+
+const toArrayRestOperatorRefactores = (...args) => args;
+
+console.log(toArrayRestOperator(1,2,3,4,5));
+console.log(toArrayRestOperatorRefactores(1,2,3,4,5));
