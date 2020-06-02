65fe1fe8c631847acc8fe9d402d4507c18c6a434
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-02 11:55:26 -0300

    C01A08 - Working with the REPL vs Using Files

--- first-app.js
+++ first-app.js
@@ -3,3 +3,15 @@ const fs = require('fs');
 console.log('Hello from Node.js!');
 
 fs.writeFileSync('hello.txt', 'Hello from Node.js!')
+
+// REPL
+
+// R Read      ==> Read User Input
+// E Evaluate  ==> Evaluate User Input
+// P Print     ==> Print Output (Result)
+// L Loop      ==> Wait for New Input
+
+// To acess, just type "node" in your terminal
+// You can run commands, import packages
+// But when you leave, it is not saved anywhere
+// To exit, press Ctrl + C twice
