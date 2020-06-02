02834b374029c5996279139200517a7e6dc5ece8
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-02 18:36:21 -0300

    C02A21 - Template Literals

--- play.js
+++ play.js
@@ -257,7 +257,7 @@ console.log(hobby1, hobby2);
 */
 
 // Asynchronous Code
-
+/*
 setTimeout(() => {
     console.log('Timer is done!');
 }, 2000);
@@ -322,3 +322,27 @@ setTimeout(() => {
 }, 2000);
 
 // this is more readable, right?
+*/
+
+// Template Literals
+
+// One other feature, we'll use from time to time are template literals:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+// It's a different way of writing strings.
+// Instead of using double or single quotation marks: 'A String'
+// or:  "Another string"
+// you can use backticks (`): `Another way of writing strings`
+
+// Now why would we use that way of creating strings?
+// With that syntax, you can dynamically add data into a string like this:
+
+const name = "Max";
+const age = 29;
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+// This is of course shorter and easier to read than the "old" way of concatenating strings:
+
+const name = "Max";
+const age = 29;
+console.log("My name is " + name + " and I am " + age + " years old.");
