85a0b795ae3eca9bffb336f67ce8a8344f17b5c7
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-02 19:17:39 -0300

    C03A27 - The Node LifeCycle & Event Loop

--- app.js
+++ app.js
@@ -6,10 +6,12 @@
 // path  --> a helper to work with url between different operating systems
 // os    --> 
 
+/*
 // imports the http module
 const http = require('http');
 
 // this function will respond to whatever requests reach our server
+
 function requestListener(req, res) {
     console.log(req);
 }
@@ -18,22 +20,35 @@ function requestListener(req, res) {
 const server = http.createServer(requestListener);
 
 server.listen(3000);
+*/
 
 /////////////////////////////////////////////
 
 // or you can create an anonymous function
 // this means the same
+/*
+// imports the http module
+const http = require('http');
+
 const server = http.createServer(function(req, res) {
     console.log(req);
 });
 
 server.listen(3000);
+*/
 
 /////////////////////////////////////////////
 
 // or you can even do that:
+
+// imports the http module
+const http = require('http');
+
 const server = http.createServer((req, res) => {
     console.log(req);
+
+    // this breaks the event loop
+    process.exit();
 });
 
 server.listen(3000);
