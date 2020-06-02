54f66b523f70c1f8c072440007bc88dd485a3aca
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-02 20:21:26 -0300

    C03A33 - Redirecting Requests

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-    
+    const method = req.method;
+
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Hello from the server!</title></head>');
-        res.write('<body><form action="/message" method="post"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         // we must do this in order not to execute the outer lines
         return res.end();
     }
     
+    if (url === '/message' && method === 'POST') {
+        const message = 'DUMMY TEXT';
+        // I want to store the message in a text file
+        fs.writeFileSync('message.txt', message);
+        // And redirect the user back to '/'
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page!</title></head>');
