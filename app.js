3e2448cb88e1cfd1dd76c8eca45ee776920b735b
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-02 22:16:24 -0300

    C04A45 - Using Nodemon for Autorestarts

--- app.js
+++ app.js
@@ -54,13 +54,13 @@ server.listen(3000);
 // Global features are always available, you don't need to import them into the files where you want to use them.
 // Core Node.js Modules don't need to be installed (NO npm install is required) but you need to import them when you want to use features exposed by them.
 // Example:
-const fs = require('fs');
+// const fs = require('fs');
 // You can now use the fs object exported by the "fs" module.
 
 // Third-party Modules need to be installed (via npm install in the project folder) AND imported.
 // Example (which you don't need to understand yet - we'll cover this later in the course):
 
 // In terminal/ command prompt
-npm install express-session --save
+// npm install express-session --save
 // In code file (e.g. app.js)
-const sessions = require('express-session');
+// const sessions = require('express-session');
