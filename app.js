460c7654819fd9d0180cbcab37fabcfc2ca037de
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-04 20:47:33 -0300

    C05A63 - Handling Different Routes

--- app.js
+++ app.js
@@ -1,57 +1,29 @@
-// const http = require('http');
-
-// how to install express.js?
-// npm install express --save
-
-// how to use express?
-// 1) Import express.js
 const express = require('express');
 
-// 2) create an express application
 const app = express();
-// this will initialize a new object 
-// where express.js the framework
-// will store and manage a lot of
-// things for us behind the scenes
 
-// and now you can use it! \o/
+// how to handle different routes?
 
-// and how do I use it?
-// well, good question!
-// express.js is all about middleware
-// request --> middleware1(req, res, next) --> next() --> middleware2(req, res, next) --> next() ... res.send() --> response
-// and that's it in a nutshell!
-
-// how to create a middleware?
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); // allows the request to continue to the next middleware in line
+// this handles the '/' request
+// but what if a request the '/help' page?
+// this triggers too
+// why?
+// because this means that the request must start with '/'
+// so, '/help' is also valid
+// and what to do if you want to display a different page for '/help?'
+// place the '/help' handler before the '/'
+// and what if I want to use a middleware that runs before both handlers?
+app.use('/', (req, res, next) => {
+    console.log('I am a middleware!');
+    next(); // don't forget the next() function!
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
-    next(); // allows the request to continue to the next middleware in line
+app.use('/help', (req, res, next) => {
+    res.send('<h1>Ja ja ja, Was ist los? Was ist das?</h1>');
 });
 
-app.use((req, res, next) => {
-    console.log('In yet another middleware');
-    // res.setHeader('Content-Type', 'text/html'); // you still can call res.setHeader()
-    // res.write('<h1>Okay, you got it, right?</h1>'); // you can still call res.write()
-    // but now express.js makes things simpler with the function res.send()
-    res.send('<h1>Okay, you got it, right?</h1>'); // res.send finishes the request and sends the response
-    // by default it sets the Content-Type to text/html everytime it detects a string in the response
-    // you can override this by using res.setHeader() of course
-    // and also you don't need to use res.end() anymore.
+app.use('/', (req, res, next) => {
+    res.send('<h1>Eins Zwei Polizei!</h1>');
 });
 
-// we can shorten this code here
-// we can pass app the createServer method
-// but we can also app.listen
-// and this will do both this things for us
-// the app.listen method checks if we have already created a server
-// if not, it creates for us, so this code below 
-// const server = http.createServer(app);
-// server.listen(3000);
-// can be replaced by
 app.listen(3000);
-// and now we don't even need to import the http module anymore
