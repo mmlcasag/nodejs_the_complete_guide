cc4f7618a163dc5da446f078629e30c23c8c15b3
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-04 21:43:43 -0300

    C05A67 - Adding a 404 Error Page

--- app.js
+++ app.js
@@ -3,18 +3,27 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// how to place all those routes into different files?
-// i'll create a new folder called "routes"
-// this two new files: admin.js e shop.js
-// '/add-product' and '/product' will go to admin.js
-// '/' will go to shop.js
-
-// now i have to import the newly created pages here
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+// notice that when using app.get or app.post
+// now the handler only triggers when there's an exact corresponce
+// between the url name and the filter name
+// but what if i want back that functionality for every request where url is like '%...%'?
+// switch back to app.use()
+
+// what if i want to create a 404 page to answer for all other requests?
+app.use((req, res, next) => {
+    // it would be nice to also return an http status of 404
+    res.status(404);
+    // and print something as a response
+    res.send('<h1>Page not found</h1>');
+    // you can also concatenate the functions like that:
+    // res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,7 @@
-// you have to import express again here
 const express = require('express');
 
-// this is like a mini express app
-// pluggable to the main app
-// which i can export it to the main app
 const router = express.Router();
 
-// you just have to change app for router
-// remember? app is the app
-// router is a mini app
-// so replace app for router
-// and then export the router to be used by app
 router.get('/add-product', (req, res, next) => {
     res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');
 });
@@ -20,5 +11,4 @@ router.post('/product', (req, res, next) => {
     res.redirect('/'); 
 });
 
-// here I export it
 module.exports = router;
