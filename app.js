3b744300b9ef655e4b7097eb91280094c5894cc5
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-04 21:50:45 -0300

    C05A68 - Filtering Paths

--- app.js
+++ app.js
@@ -7,23 +7,17 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(adminRoutes);
+
+// what if i want to add a '/admin' before every route inside admin.js?
+// instead of going one by one inside the file
+// you can do it like that
+app.use('/admin', adminRoutes);
+// now you have to access /admin/add-product
+// and submit to /admin/product
 app.use(shopRoutes);
 
-// notice that when using app.get or app.post
-// now the handler only triggers when there's an exact corresponce
-// between the url name and the filter name
-// but what if i want back that functionality for every request where url is like '%...%'?
-// switch back to app.use()
-
-// what if i want to create a 404 page to answer for all other requests?
 app.use((req, res, next) => {
-    // it would be nice to also return an http status of 404
-    res.status(404);
-    // and print something as a response
-    res.send('<h1>Page not found</h1>');
-    // you can also concatenate the functions like that:
-    // res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
-    res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');
+    res.send('<html><body><form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>');
 });
 
+// POST /admin/product
 router.post('/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/'); 
