eb482561299349a43093b786a8e911d2fddae009
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-04 22:35:21 -0300

    C05A72 - Using a Helper Function for Navigation

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// so what if i want to add a html file for our 404 page?
-// first lets create a file named 404.html
-// then create a route named error.js
-// then import it here
 const errorRoutes = require('./routes/error');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// instead of using __dirname and then .. to go up one level
+// there's ine better way to solve this
+// and this is by using a helper
+// so i'm creating a utils folder and a file named root.js
+// and i'll import it on my routes and replace the path joins
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
-// then register it here 
-// here it has to be placed in the last position
 app.use(errorRoutes);
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,18 @@ const express = require('express');
 
 const path = require('path');
 
+// importing our root file
+const root = require('../utils/root');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    const dirfile = path.join(__dirname, '..', 'views', 'add-product.html');
+    //const dirfile = path.join(__dirname, '..', 'views', 'add-product.html');
+
+    // now we can replace the line above to this one:
+    const dirfile = path.join(root, 'views', 'add-product.html');
+    // much better, right?
+    
     res.sendFile(dirfile);
 });
 
