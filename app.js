0be9d795582ff42d02103989d2dd6c0081a0eee1
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-05 19:31:42 -0300

    C06A81 - Installing & Implementing Pug

--- app.js
+++ app.js
@@ -10,6 +10,19 @@ const errorRoutes = require('./routes/error');
 
 const app = express();
 
+// to register our template engine for the project
+// we have to set a global configuration value
+// so we are basically saying this to express.js
+// for any dynamic templates we are try to render, please use this one
+app.set('view engine', 'pug');
+// we also need to tell express the folder where our views are
+app.set('views', 'views');
+// now lets add dynamic templates
+// look to all the .pug files inside the views folder
+
+// after that, in shop.js we have to render our dynamic template
+// so go over there to see what we have done
+
 app.use(express.static(path.join(root, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/admin', admin.routes); // this is what we adapted
--- routes/shop.js
+++ routes/shop.js
@@ -15,8 +15,24 @@ router.get('/', (req, res, next) => {
     // it works!
     // so this is one way to share data
     // but doing this way means the data is shared across requests and users
-    const dirfile = path.join(root, 'views', 'shop.html');
-    res.sendFile(dirfile);
+    
+    // so now let's install all three template engines to see how we can work with it
+    // and display our view dinamically with our products
+    // npm install pug --save
+    // npm install ejs --save
+    // npm install express-handlebars --save
+
+    // after installed let's go our app.js and register our template engine
+
+    // after creating our dynamic templates, we need to render it
+    // so this:
+    // const dirfile = path.join(root, 'views', 'shop.html');
+    // res.sendFile(dirfile);
+    // becomes this:
+    res.render('shop');
+    // we have already told express that our dynamics templates are in th views folder
+    // we have already told express that our dynamics templates should be interpreted with pug
+    // so by passing just 'shop' it already know that we mean to access our views/shop.pug file
 });
 
 module.exports = router;
