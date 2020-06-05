5b08188b300f1a1671d5b8d5e869e1d7dd972c0f
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-04 23:05:54 -0300

    C05T03 - Time to Practice - Navigation

--- app.js
+++ app.js
@@ -1,35 +1,20 @@
+// create a npm project and install express.js and nodemon if you want
+
+// create an express.js app which serves two html files of your choice for '/' and '/users'
+
+// add some static css files to your project that should be required by at least one of your html files
+
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyparser = require('body-parser');
 
 const path = require('path');
-const root = require('./utils/root');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const errorRoutes = require('./routes/error');
+const routes = require('./routes');
 
 const app = express();
 
-// this lecture shows how to style our pages
-// so basically all that i did was to the edit all html files
-// placing some css inside
-// but as you can see we are copying and pasting in every page
-// now we want to use external css files and import them to our pages
-// the convention is to create a folder named public
-// remember: all your folders and files are not exposed to the public
-// if you type localhost:3000/routes/admin.js express doesn't show the file
-// but this is exactly what we want to do with public js and css files
-// so now we have to tell express which folder(s) we want to expose statically
-app.use(express.static(path.join(root, 'public')));
-
-// then i removed the css from inside the html files
-// placed them inside the css files located at /public/css/main.css and /public/css/product.css
-// and switched from <style> tag to <link rel> tag, importing the newly created public css files
-// OBS: notice that on the <link rel> we don't use the /public folder
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-app.use(errorRoutes);
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 app.listen(3000);
---
+++ routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const path = require('path');
+
+const router = express.Router();
+
+router.get('/users', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'users.html'));
+});
+
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+router.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+module.exports = router;
