35a7aa142e48ee034eb8741caf72a48a4e69b6ac
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-05 21:50:46 -0300

    C06A89 - Converting our Project to Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -5,7 +5,13 @@ const admin = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('shop', { pageTitle: 'Shop Page', path: '/', products: admin.products });
+    const attributes = {
+        pageTitle: 'Shop Page',
+        path: '/',
+        products: admin.products,
+        hasProducts: admin.products.length > 0 // handlebars forbids logic inside the view
+    };
+    res.render('shop', attributes);
 });
 
 module.exports = router;
