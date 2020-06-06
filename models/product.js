7fff8fcfb0daf471c7f3784cd2a6396863be9d8c
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-06 09:25:20 -0300

    C07A101 - Storing Data in Files Via the Model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,29 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const root = require('../utils/root');
+
+const fileDirectory = path.join(root, 'data', 'products.json');
+
+const readContentFromFile = (fileDirectory, callback) => {
+    fs.readFile(fileDirectory, (err, fileContent) => {
+        if (err) {
+            callback([]);
+        } else if (fileContent.length === 0) {
+            callback([]);
+        } else {
+            callback(JSON.parse(fileContent));
+        }
+    });
+}
+
+const writeContentToFile = (fileDirectory, fileContent) => {
+    fs.writeFile(fileDirectory, JSON.stringify(fileContent), (err) => {
+        if (err) {
+            console.log('models.product: Error while trying to write file: ' + err);
+        }
+    });
+}
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +31,21 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        readContentFromFile(fileDirectory, fileContent => {
+            const products = [...fileContent];
+            products.push(this);
+            writeContentToFile(fileDirectory, products);
+        });
     }
 
     static fetchAll() {
-        return products;
+        /*
+        return readContentFromFile(fileDirectory, fileContent => {
+            const products = [...fileContent];
+            console.log(products);
+            return products;
+        });
+        */
+        return [];
     }
 }
