eac75cd7a8a24eb44edaa22eeb2bb7cc7291fc56
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-08 18:30:20 -0300

    C11A157 - Getting a Single Product with the WHERE Condition

--- controllers/shop.js
+++ controllers/shop.js
@@ -35,17 +35,29 @@ module.exports.getProducts = (req, res, next) => {
 
 module.exports.getProductDetails = (req, res, next) => {
     const id = req.params.id;
-    Product.loadById(id)
-        .then(([product]) => {
+    // we can use the obvious version:
+    // Product.findByPk(id)
+    // or we can also use it specifying the where clause
+    Product.findAll({ where: { id: id } })
+        // since it's findAll it returns an array of elements
+        // so we have to force to show just the first element
+        // of the products array
+        // we know that we are querying by the id
+        // but sequelize doesn't know this
+        .then(products => {
             res.render('shop/product-detail', {
-                pageTitle: product.title,
+                pageTitle: products[0].title,
                 path: '/products',
-                product: product[0]
+                product: products[0]
             });
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err);
         });
+    // of course, if we used the findByPk version
+    // we wouldn't need to do all these changes
+    // check out the postAddToCart where I provided
+    // an example using findByPk
 }
 
 module.exports.getCart = (req, res, next) => {
@@ -60,12 +72,12 @@ module.exports.getCart = (req, res, next) => {
 
 module.exports.postAddToCart = (req, res, next) => {
     const id = req.body.id;
-    Product.loadById(id)
-        .then(([product]) => {
-            Cart.add(product[0]);
+    Product.findByPk(id)
+        .then(product => {
+            Cart.add(product);
             res.redirect('/cart');
         })
-        .catch((err) => {
+        .catch(err => {
             console.log(err);
         });
 }
