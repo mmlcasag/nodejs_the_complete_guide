bdeb19f764e9bdc73216d59468ec7a7fc0e93284
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-12 19:57:23 -0300

    C15A253 - Our Updated Project Status

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-// ok, managing sessions seems fine the way it is
-// but now we are storing session data on the server memory
-// if your application has many users logged in simultaneously
-// this will become a problem
-// the best place to store sessions is in the database
-// we will store our sessions in mongodb
-// for that we need to install another third-party package
-// npm install --save connect-mongodb-session
-
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-// after the session import, let's import the connect-mongodb-session
-// you also have to pass your session as an argument of the returned function
 const MongoDBStore = require('connect-mongodb-session')(session);
 
 const User = require('./models/user');
@@ -30,12 +19,7 @@ const app = express();
 
 const MONGODB_URI = 'mongodb+srv://admin:admin@mmlcasag-cvtew.mongodb.net/mongoose';
 
-// after that, you can initialize your session store
-// passing some configuration in the constructor
-const store = new MongoDBStore({
-    uri: MONGODB_URI, // what database should it store that information?
-    collection: 'sessions' // in which collection should the information be stored?
-});
+const store = new MongoDBStore({ uri: MONGODB_URI, collection: 'sessions' });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -43,13 +27,7 @@ app.set('views', 'views');
 app.use(express.static(path.join(root, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// and since we now have a place to store our sessions
-// we need to pass this in the session configuration as a new argument
 app.use(session({ secret: 'BARIPOAUJFGVPSF', resave: false, saveUninitialized: false, store: store }));
-// and that's it!
-// now all session information will be stored in the database!
-// this is much more efficient and secure
-// always do that in your future applications!
 
 app.use((req, res, next) => {
     if (req.session.user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,17 @@
 const User = require('../models/user');
 
+module.exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        pageTitle: 'Sign Up',
+        path: '/auth/signup',
+        isLoggedIn: req.session.isLoggedIn
+    });
+}
+
+module.exports.postSignup = (req, res, next) => {
+    
+}
+
 module.exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         pageTitle: 'Login',
@@ -9,25 +21,6 @@ module.exports.getLogin = (req, res, next) => {
 }
 
 module.exports.postLogin = (req, res, next) => {
-    // our session package added this session object
-    // and here we can add any key we want
-    // for example:
-    // req.session.isLoggedIn = true;
-    // you can see after sign in that the session package
-    // created a cookie with your session id
-    // this value is just for you and will be valid until you close your browser
-    
-    // you can see here your session object with the attribute we just added
-    // console.log(req.session);
-
-    // this is all very good
-    // but how do I access the information I recorded in the session?
-    // const isLoggedIn = req.session.isLoggedIn;
-    // okay! so now let's adapt how we send the loggedIn information for our views
-
-    // ACTIVITY 5
-    // after the user logged in
-    // store the user object in the session
     User.findById('5ee23237956ed626eca64fca')
         .then(user => {
             req.session.user = user;
@@ -42,15 +35,10 @@ module.exports.postLogin = (req, res, next) => {
 }
 
 module.exports.postLogout = (req, res, next) => {
-    // here I want to clean out my session
-    // how do I do that?
     req.session.destroy(err => {
-        // I also want to delete the cookie
-        // how do I do that?
-        // you don't do it!
-        // the purpose of the cookie is to communicate with the session in the database
-        // since there's no session in the database anymore, the cookie is now invalid
-        // also, when you close the browser, the cookie will be deleted
+        if (err) {
+            console.log(err);
+        }
         res.redirect('/');
     });
 }
