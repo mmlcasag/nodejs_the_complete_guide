8654cbc28c00edcfd073f1265a975d91d12bf0a5
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-12 21:39:19 -0300

    C15A255 - Encrypting Passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 module.exports.getSignup = (req, res, next) => {
@@ -24,14 +26,28 @@ module.exports.postSignup = (req, res, next) => {
                 return res.redirect('/auth/login');
             // if it doesn't, it means there's no user yet and so this one can proceed
             } else {
-                // so let's create it!
-                const newUser = new User({
-                    email: email,
-                    password: password,
-                    cart: { items: [] }
-                }) ;
-                return newUser.save();
+                // before saving the new user we need to work on the password
+                // storing password without encryption is a huge security flaw!!!
+                // we must always encrypt the password
+                // to do that, let's import the bcryptjs package
+                // npm install --save bcryptjs
+                // bcryptjs helps us encrypting our passwords
+                // first argument is the string you want to encrypt
+                // second argument is the number of rounds
+                // the higher the more secure, but also takes more time
+                // also it returs a promise
+                // to let's return it to nest another then() block
+                return bcrypt.hash(password, 10);
             }
+        })
+        .then(hashedPassword => {
+            // now let's create it!
+            const newUser = new User({
+                email: email,
+                password: hashedPassword,
+                cart: { items: [] }
+            });
+            return newUser.save();
         })
         .then(result => {
             // after signing up, we want to redirect the user to the login page
