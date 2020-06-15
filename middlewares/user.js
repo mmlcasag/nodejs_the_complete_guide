a9572b1b9baf9e33eaf2d02ca5caba051be8717e
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-15 19:10:09 -0300

    C19A309 - Throwing Errors in our Code

--- middlewares/user.js
+++ middlewares/user.js
@@ -4,13 +4,23 @@ module.exports = (req, res, next) => {
     if (req.session.user) {
         User.findById(req.session.user._id)
             .then(user => {
+                // we added an extra check
+                // we could have a user stored in the session
+                // but not anymore in the database
+                // therefore, there might no return an user to be returned here
+                if (!user) {
+                    return next();
+                }
                 req.user = user;
                 next();
             })
             .catch(err => {
-                console.log(err);
+                // just logging the error is not that useful
+                // it is better to throw another error
+                // so it can be handled by some other method
+                throw new Error(err);
             });
     } else {
-        next();
+        return next();
     }
 }
