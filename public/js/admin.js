1005cb228ba99c9bb21d1f588df8839df1e83ae5
Marcio Casagrande <mmlcasag@gmail.com>
2020-06-17 11:21:25 -0300

    C22A351 - Manipulating the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -5,6 +5,7 @@ const deleteProduct = (btn) => {
     console.log(id);
     console.log(_csrf);
 
+    // this fetch request returns a promise
     fetch('/admin/product/' + id, {
         method: 'DELETE',
         // we need to pass the csrf somehow
@@ -14,9 +15,28 @@ const deleteProduct = (btn) => {
             'csrf-token': _csrf
         }
     })
-    // this request returns a promise
+    // so we handle it here
     .then(result => {
+        // as you can see in the log it doesn't really return my response
+        // instead it returns a lot of stuff
         console.log(result);
+        // in order to get my response, we need to do that
+        // and this json() method also returns a promise
+        return result.json();
+    })
+    // so we handle it here
+    .then(data => {
+        // and here it is my response
+        console.log(data);
+
+        // and now we need to remove the entire element from the screen
+        // because we have already deleted the document from the database
+        // so first we need to fetch the entire "tr" tag and its content
+        const productRow = btn.closest('tr');
+        console.log(productRow);
+        // and this is how we remove the element from the DOM
+        // we need to go up one level and then remove the child element
+        productRow.parentNode.removeChild(productRow);
     })
     .catch(err => {
         console.log(err);
